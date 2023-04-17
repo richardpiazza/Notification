11dac79d423aab92b9e6def1c38d16c8bac17ede
Richard Piazza <richard@richardpiazza.com>
2023-04-17 15:18:55 -0500

    Typealias for 'AuthorizationStatus'

--- Sources/Notification/AuthorizationStatus.swift
+++ Sources/Notification/AuthorizationStatus.swift
@@ -1,5 +1,8 @@
 import Foundation
 
+/// Alias to `AuthorizationStatus` to allow disambiguation when `Notification.AuthorizationStatus` cannot be used.
+public typealias NotificationAuthorizationStatus = AuthorizationStatus
+
 public enum AuthorizationStatus: Codable {
     /// The user has not yet made a choice regarding whether the application may post user notifications.
     case notDetermined
