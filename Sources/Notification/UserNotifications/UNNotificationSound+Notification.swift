4f5bb3e2688b86fe4f1c1140a2e9a3daea3c100e
Richard Piazza <richard@richardpiazza.com>
2026-09-06 10:53:35 -0500

    WatchOS Sounds

--- Sources/Notification/UserNotifications/UNNotificationSound+Notification.swift
+++ Sources/Notification/UserNotifications/UNNotificationSound+Notification.swift
@@ -5,15 +5,27 @@ public extension UNNotificationSound {
     static func make(with notificationSound: UserNotification.Sound) -> UNNotificationSound {
         switch notificationSound {
         case .critical(.some(let name), .some(let volume)):
+            #if os(watchOS)
+            .defaultCriticalSound(withAudioVolume: volume)
+            #else
             .criticalSoundNamed(UNNotificationSoundName(name), withAudioVolume: volume)
+            #endif
         case .critical(.some(let name), .none):
+            #if os(watchOS)
+            .defaultCritical
+            #else
             .criticalSoundNamed(UNNotificationSoundName(name))
+            #endif
         case .critical(.none, .some(let volume)):
             .defaultCriticalSound(withAudioVolume: volume)
         case .critical(.none, .none):
             .defaultCritical
         case .named(let name):
+            #if os(watchOS)
+            fallthrough
+            #else
             UNNotificationSound(named: UNNotificationSoundName(name))
+            #endif
         default:
             .default
         }
